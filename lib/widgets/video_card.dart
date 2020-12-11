d25ee3b7b0336463703be1164d3c7dc0dc13f251
amansharma2910 <amansharma2910@gmail.com>
2020-12-12 02:20:20 +0530

    YT card complete

--- lib/widgets/video_card.dart
+++ lib/widgets/video_card.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class VideoCard extends StatelessWidget {
@@ -10,10 +11,10 @@ class VideoCard extends StatelessWidget {
       child: Stack(
         children: [
           Container(
-            margin: EdgeInsets.all(30),
+            margin: EdgeInsets.all(20),
             decoration: BoxDecoration(
-                color: Color(0xffb08613),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
+              color: Color(0xffb08613),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
             ),
             child: Container(
               margin: EdgeInsets.all(1.5),
@@ -21,9 +22,69 @@ class VideoCard extends StatelessWidget {
                 color: Colors.black,
                 borderRadius: BorderRadius.all(Radius.circular(10)),
               ),
+              child: Stack(
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage('images/yt_thumbnail.png')),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.5),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 120,
+                            height: 120,
+                            child: PlayButton(),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class PlayButton extends StatefulWidget {
+  @override
+  _PlayButtonState createState() => _PlayButtonState();
+}
+
+class _PlayButtonState extends State<PlayButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: 60,
+      decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.4),
+          borderRadius: BorderRadius.all(Radius.circular(120))),
+      child: IconButton(
+        alignment: Alignment.center,
+        icon: Icon(
+          Icons.play_arrow,
+          color: Colors.white54,
+        ),
+        iconSize: 100,
       ),
     );
   }
