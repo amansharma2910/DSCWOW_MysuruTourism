111124438a22dd0c738570f6e8cd60d8cf00d48f
amansharma2910 <amansharma2910@gmail.com>
2020-12-11 20:49:21 +0530

    Added banner image

--- lib/widgets/welcome_banner.dart
+++ lib/widgets/welcome_banner.dart
@@ -3,10 +3,38 @@ import 'package:flutter/material.dart';
 class WelcomeBanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        BackgroundImage(),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.55,
+        ),
+      ],
+    );
+  }
+}
+
+
+class BackgroundImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      height: 300,
       width: MediaQuery.of(context).size.width,
-      color: Colors.redAccent,
+      height: MediaQuery.of(context).size.height * 0.55,
+      child: FittedBox(
+        fit: BoxFit.cover,
+        alignment: Alignment.center,
+        child: ColorFiltered(
+          colorFilter: ColorFilter.mode(
+            Color(0x23ba8d3a),
+            BlendMode.color,
+          ),
+          child: Image(
+            image: AssetImage('images/mysuru_banner.jpg'),
+          ),
+        ),
+      ),
     );
   }
 }
