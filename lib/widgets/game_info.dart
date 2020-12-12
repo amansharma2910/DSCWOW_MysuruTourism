f8420c0df2b56d056212a2d33d5bc3fd2e5b8ca6
amansharma2910 <amansharma2910@gmail.com>
2020-12-12 16:19:11 +0530

    Working on banners

--- lib/widgets/game_info.dart
+++ lib/widgets/game_info.dart
@@ -3,18 +3,47 @@ import 'package:flutter/material.dart';
 class InfoCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: MediaQuery.of(context).size.width,
-      alignment: Alignment.center,
-      color: Colors.greenAccent,
-      child: Text(
-        'Placeholder',
-        style: TextStyle(
-          fontSize: 25.0,
-          fontWeight: FontWeight.w300,
+    return Stack(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 300,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+    colorFilter: new ColorFilter.mode(
+    Colors.black.withOpacity(0.8), BlendMode.lighten),
+    fit: BoxFit.cover,
+    image: AssetImage('images/game_info_banner.jpg'),
+            )
+          ),
         ),
-      ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 300,
+          decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.8),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 300,
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '#LiveMysuru Challenge',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
